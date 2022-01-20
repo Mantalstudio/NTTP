5df50444c98a4155ba4315269a590cef9510dbb8
M4RMU <97147718+B4BY-DG@users.noreply.github.com>
2022-01-20 21:44:54 +0630

    Update kit.py

--- kit.py
+++ kit.py
@@ -81,8 +81,8 @@ print  """\033[1;91m=======================================
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "dedsec"
-CorrectPassword = "dedsec"
+CorrectUsername = "kit"
+CorrectPassword = "kit"
 
 loop = 'true'
 while (loop == 'true'):
